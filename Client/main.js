a524162725c2745a0529ea70abcaafb7fc1560d0
Ignasi García Fernández <ignasi.garcia@outlook.es>
2017-09-17 18:05:05 +0200

    Aclaración del arraque del servidor y moidificación del Js para agregar que borre el mensaje al enviarlo

--- Client/main.js
+++ Client/main.js
@@ -1,33 +1,34 @@
-var socket = io.connect('http://192.168.1.47:7077', {'forceNew':true});
+var socket = io.connect('http://192.168.1.47:7077', { 'forceNew': true });
 
-socket.on('messages', function(data){
-  console.log(data);
-  render(data);
+socket.on('messages', function(data) {
+    console.log(data);
+    render(data);
 });
 
-function render(data){
-  var html = data.map(function(message, index){
-    return (`
+function render(data) {
+    var html = data.map(function(message, index) {
+        return (`
       <div class="message">
         <strong>${message.nickname}:</strong>
         <p>${message.text}</p>
       </div>
       `);
-  }).join(' ');
+    }).join(' ');
 
-  var div_msgs = document.getElementById('messages');
-  div_msgs.innerHTML = html;
-  div_msgs.scrollTop = div_msgs.scrollHeight;
+    var div_msgs = document.getElementById('messages');
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
 }
 
-function addMessage(e){
-  var message = {
-    nickname: document.getElementById('nickname').value,
-    text: document.getElementById('text').value
-  };
+function addMessage(e) {
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
 
-  document.getElementById('nickname').style.display = 'none';
-  socket.emit('add-message', message);
+    document.getElementById('nickname').style.display = 'none';
+    document.getElementById('text').value = "";
+    socket.emit('add-message', message);
 
-  return false;
+    return false;
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # chat-nodeJs-socketIO
-Chat elaborado con NodeJs y SocketIO
+Chat elaborado con NodeJs y SocketIO y maquetado con Bootstrap.
+
+## Para arrancar el servidor
+
+Simplemente posicionarse en la carpeta y ejecutar el comando:
+
+```npm start```
+
